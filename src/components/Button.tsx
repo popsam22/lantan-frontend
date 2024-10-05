9ebd6e5ee5c4894c99090a7a52dc0e78bc4ae5f3
popsam22 <sobiechie7@gmail.com>
2024-10-05 23:15:13 +0100

    fix: add document button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,14 +1,16 @@
 import { ButtonProps } from "../types";
 import clsx from "clsx";
+import addDocument from "../assets/Plus.png";
 
 const Button = ({ label, bgColor }: ButtonProps) => {
   return (
     <button
       className={clsx(
-        "w-[146px] h-[34px] text-[#206AB2] font-semibold text-xs whitespace-nowrap rounded-lg bg-[#075AAA] hover:cursor-pointer",
+        "flex justify-evenly items-center w-[173px] h-[34px] p-2 text-[#206AB2] font-semibold text-xs whitespace-nowrap rounded-lg bg-[#075AAA] hover:cursor-pointer",
         bgColor && "text-[#206AB2] bg-[#F1F6FE]"
       )}
     >
+      <img src={addDocument} alt="add document" width={16} height={16} />
       {label}
     </button>
   );
